a3a58f9527756d42083d7c3fabc197a69326d34a
xiayuushi <xiayuushi@qq.com>
2021-10-26 01:44:24 +0800

    首页模块 - 抽取头部导航组件

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,9 +1,11 @@
-import AppNav from '../components/appNav'
-import AppHeader from '../components/appHeader'
-import AppFooter from '../components/appFooter'
+import AppNav from '../components/AppNav'
+import AppHeader from '../components/AppHeader'
+import AppFooter from '../components/AppFooter'
+import AppHeaderNav from '../components/AppHeaderNav'
 
 export default function (Vue, options) {
   Vue.component(AppNav.name, AppNav)
   Vue.component(AppHeader.name, AppHeader)
   Vue.component(AppFooter.name, AppFooter)
+  Vue.component(AppHeaderNav.name, AppHeaderNav)
 }
