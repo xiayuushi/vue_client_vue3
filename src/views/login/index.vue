1b0edfa5b51759682c47b7ba2189c44f77102300
xiayuushi <xiayuushi@qq.com>
2021-11-18 19:11:09 +0800

    登录模块 - 基础布局与切换效果

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,88 @@
 <template>
-  <div>登录</div>
+  <LoginHeader>欢迎登录</LoginHeader>
+  <section class="login-section">
+    <div class="wrapper">
+      <nav>
+        <a @click="type = 'account'" :class="{ active: type === 'account' }" href="javascript:;">账户登录</a>
+        <a @click="type = 'qrcode'" :class="{ active: type === 'qrcode' }" href="javascript:;">扫码登录</a>
+      </nav>
+      <!-- 表单 -->
+      <LoginForm v-if="type === 'account'" />
+      <!-- 二维码 -->
+      <div v-if="type === 'qrcode'" class="qrcode-box">
+        <img src="@/assets/images/qrcode.jpg" alt="">
+        <p>打开 <a href="javascript:;">小兔鲜App</a> 扫码登录</p>
+      </div>
+    </div>
+  </section>
+  <LoginFooter />
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
+import LoginForm from './components/login_form'
+import LoginHeader from './components/login_header'
+import LoginFooter from './components/login_footer'
 
+export default {
+  name: 'Login',
+  components: { LoginHeader, LoginFooter, LoginForm },
+  setup () {
+    const type = ref('account')
+    return { type }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login-section {
+  background: url(../../assets/images/login-bg.png) no-repeat center / cover;
+  height: 488px;
+  position: relative;
+  .wrapper {
+    width: 380px;
+    background: #fff;
+    min-height: 400px;
+    position: absolute;
+    left: 50%;
+    top: 54px;
+    transform: translate3d(100px,0,0);
+    box-shadow: 0 0 10px rgba(0,0,0,.15);
+    nav {
+      height: 55px;
+      border-bottom: 1px solid #f5f5f5;
+      display: flex;
+      padding: 0 40px;
+      text-align: right;
+      align-items: center;
+      a {
+        flex: 1;
+        line-height: 1;
+        display: inline-block;
+        font-size: 18px;
+        position: relative;
+        &:first-child {
+          border-right: 1px solid #f5f5f5;
+          text-align: left;
+        }
+        &.active{
+          color: @xtxColor;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+}
+// 二维码容器
+.qrcode-box {
+  text-align: center;
+  padding-top: 40px;
+  p {
+    margin-top: 20px;
+    a {
+      color: @xtxColor;
+      font-size: 16px;
+    }
+  }
+}
 </style>
