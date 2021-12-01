f2bcb917083b5453a8385fef93be05ddf456f3e1
xiayuushi <xiayuushi@qq.com>
2021-12-01 22:28:18 +0800

    支付模块 - 跳转支付

--- src/views/member/pay/index.vue
+++ src/views/member/pay/index.vue
@@ -25,7 +25,7 @@
         <div class="item">
           <p>支付平台</p>
           <a class="btn wx" href="javascript:;"></a>
-          <a class="btn alipay" href="javascript:;"></a>
+          <a class="btn alipay" :href="aLiPayHref" target="_blank" @click="visibleDialog=true"></a>
         </div>
         <div class="item">
           <p>支付方式</p>
@@ -36,6 +36,18 @@
           <a class="btn" href="javascript:;">交通银行</a>
         </div>
       </div>
+      <!-- 支付提示对话框 -->
+      <XxxDialog title="正在支付..." v-model:visible="visibleDialog">
+      <div class="pay-wait">
+        <img src="@/assets/images/load.gif" alt="">
+        <div v-if="prePayInfo">
+            <p>如果支付成功：</p>
+            <RouterLink :to="`/member/order/${$route.query.id}`">查看订单详情></RouterLink>
+            <p>如果支付失败：</p>
+            <RouterLink to="/">查看相关疑问></RouterLink>
+        </div>
+      </div>
+    </XxxDialog>
     </div>
   </div>
 
@@ -47,6 +59,7 @@ import { useRoute } from 'vue-router'
 import { getMyMemberOrder } from '@/api/order'
 import { useIntervalFn } from '@vueuse/core'
 import dayjs from 'dayjs'
+import { baseURL } from '@/utils/request'
 
 export default {
   name: 'PayPage',
@@ -72,12 +85,27 @@ export default {
     }, 1000, { immediate: false })
     onUnmounted(() => pause())
 
-    return { prePayInfo, timeText }
+    const cbURL = encodeURIComponent('http://www.corho.com:8080/#/login/callback')
+    const aLiPayHref = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${cbURL}`
+
+    const visibleDialog = ref(false)
+
+    return { prePayInfo, timeText, aLiPayHref, visibleDialog }
   }
 }
 
 // 1、timeText.value = dayjs.unix(time.value).format('mm分ss秒') 使用dayjs的方法将服务器返回的数值转成时间戳再转成相应的'xx分xx秒'的格式
 // 2、拿到服务器返回的时间字段一开始就需要转成'xx分xx秒'的格式，因此以上代码在两个地方都写了，防止一开始从xx分58秒开始倒计时...
+// 3、为a标签添加target属性，属性值为_blank就会以新开页的形式跳转到href指定的页面
+
+// 4、跳转支付平台的a标签的href属性值完整包含以下信息：后台支付基准地址 + 接口指定的支付平台（如 /pay/aliPay） + 订单id + 回跳地址（项目静态页SDK上挂载的localhost中对应的那个线上IP地址）
+// 4、const href = `${baseURL}/pay/aliPay?orderId=${route.query.id}&redirect=http://www.corho.com:8080/#/login/callback`
+// 4、因为地址可能存在特殊字符，尽量转成编码字符进行传输，因此 将回调地址进行处理 例如：encodeURIComponent(回调地址)
+// 4、const cbUrl = encodeURIComponent('http://www.corho.com:8080/#/login/callback')
+// 4、a标签跳转支付宝支付的href属性值为： const href = `${baseURL}/pay/aliPay?orderId=${route.query.id}&redirect=${cbUrl}`
+// 4、接口指定字段分别是orderId与redirect，因此需要准备这两个字段的相应值
+// 5、沙箱测试账号 jfjbwb4477@sandbox.com 支付与登录密码都是111111
+
 </script>
 
 <style lang="less" scoped>
@@ -150,6 +178,17 @@ export default {
     &.wx {
       background: url(https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/c66f98cff8649bd5ba722c2e8067c6ca.jpg) no-repeat center / contain;
     }
+  }
+}
+.pay-wait {
+  display: flex;
+  justify-content: space-around;
+  p {
+    margin-top: 30px;
+    font-size: 14px;
+  }
+  a {
+    color: @xtxColor;
   }
 }
 </style>
